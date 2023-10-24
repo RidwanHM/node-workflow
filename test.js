a91549723749d9ad16472d87ce4717838c8ac5bc
RidwanHM <ridwan.h.mohamud@gmail.com>
2023-10-24 09:53:35 +0200

    ok

--- test.js
+++ test.js
@@ -1,3 +1,13 @@
 import "dotenv/config";
 
-console.log("Hello World");
+
+console.log(process.env.MEANING_OF_LIFE);
+
+function test(expectedSecret) {
+    const secretMatches = process.env.MEANING_OF_LIFE == expectedSecret;
+    console.log(`The meaning of life is ${process.env.MEANING_OF_LIFE}`);
+    console.log(`The secret does${secretMatches ? "" : " not"} match!`);
+    return secretMatches;
+  }
+  
+  test(42);
